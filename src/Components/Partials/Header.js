903015b5a5b5f730e052d2e75ca67e4f9dd0f8f3
Liam Ellis <liamhellis@gmail.com>
2017-10-09 17:59:14 -0400

    routing with links, new header

--- src/Components/Partials/Header.js
+++ src/Components/Partials/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AppStore from '../../Stores/AppStore'
+import {Link} from 'react-router-dom'
 
 export default class Header extends Component {
     constructor(props){
@@ -15,14 +16,32 @@ export default class Header extends Component {
         const logo_style = {
             height: '100px',
             marginBottom: '20px',
-            marginTop: '20px'
+            marginTop: '-40px'
         }
-        console.log(AppStore.data.logo)
+        const social_nav_style = {
+            textAlign: 'right',
+            marginTop: '10px'
+        }
+        const link_style = {
+            margin: '0 10px'
+        }
+        const icon_style = {
+            height: '15px',
+            width: '15px'
+        }
+        
 
         return (
             
             
                 <div className="header">
+                    <div className="social_nav">
+                        <ul className="inline-list list-inline" style={social_nav_style}>
+                            <li><a href="" style={link_style}><img src="icons/facebook.svg" style={icon_style} alt=""/></a></li>
+                            <li><a href="" style={link_style}><img src="icons/instagram.svg" style={icon_style} alt=""/></a></li>
+                            <li><a href="" style={link_style}><img src="icons/mail.svg" style={icon_style} alt=""/></a></li>
+                        </ul>
+                    </div>
                     <div className="container">
                         <div className="logo">
                            <img src={LOGO} alt="" style={logo_style} />
@@ -30,21 +49,22 @@ export default class Header extends Component {
 
                         <ul id="Nav" className="inline-list list-inline">
                             <li className="">
-                                <a href="#">Home</a>    
+                                <Link to='/'>Home</Link>
                             </li>
                             <li>
-                                <a href="#">About Selah</a>
+                                <Link to='/about'>About Selah</Link>
                             </li>
                             <li className="">
-                                <a href="#">Portfolio</a>
+                                <Link to='/portfolio'>Portfolio</Link>
                             </li>
                             <li className="">
-                                <a href="#">Beauty Tips</a></li>
+                                <Link to='/'>Beauty Tips</Link>
+                            </li>
                             <li className="">
-                                <a href="#">FAQ</a>
+                                <Link to='/faq'>FAQ</Link>
                             </li>
                             <li>
-                                <a href="#">Contact</a>
+                                <Link to='/contact'>Contact</Link>
                             </li>
                         </ul>
                     </div>
