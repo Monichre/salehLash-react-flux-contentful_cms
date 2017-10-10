206bb92ac9d78765901c78ea9b024ec447aabd7a
Liam Ellis <liamhellis@gmail.com>
2017-10-10 15:06:21 -0400

    portfolio and about and header

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,10 +1,97 @@
 import React, { Component } from 'react'
+import AppDispatcher from '../../Dispatcher/AppDispatcher'
+import AppStore from '../../Stores/AppStore'
+import ReactMarkdown from 'react-markdown'
+
+// style="background-image:url(img/about-header.jpg)">
+// style="font-size: 18px; line-height: 26px;"
+// style="background: url(img/our-story.jpg) no-repeat" 
+
 
 export default class About extends Component {
-    render(){
+    componentDidMount() {
+
+    }
+    componentWillMount() {
+        this.getPageData()
+    }
+    getPageData() {
+        AppDispatcher.dispatch({
+            action: 'get-page-data',
+            page_slug: 'About'
+        })
+    }
+    render() {
+        const page = AppStore.data.page
+        console.log(page)
+        const title = page.fields.pageTitle
+        const subTitle = page.fields.subTitle
+        const content = page.fields.pageContent
+        const image = page.fields.mainImage.fields.file.url
+        const header_style = {
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }
         return (
             <div>
-                About
+                <div className="container">
+                    <div className="header-page ef-parallax-bg" style={header_style}>
+                        <div className="col-md-6 col-md-offset-6">
+                            <div className="row">
+                                <div className="inner-content">
+                                    <div className="header-content">
+                                        <h1>{title}</h1>
+                                        <hr />
+                                        <p>{subTitle}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container margin-top">
+                    <div className="main-title near-content">
+                        <h1>WELCOME TO SELAH LASH</h1>
+                        <hr />
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <p >Who loves or pursues or desires to <b>DESIGN</b> pain of itself,but because and occasionally circumstances occur pain can procure him some great easure of the past.</p>
+                        </div>
+                        <div className="col-md-4">
+                            <p>THE CREATIVE qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima quis nostrum exercitationem ullam corporis suscipit aliquid.</p>
+                        </div>
+                        <div className="col-md-4">
+                            <p><strong>WEB DESIGN</strong> qui dolorem ipsum quia dolor sit amet, MINIMALISM adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam</p>
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="container margin-top">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="our-story wow fadeInUp" style={header_style} data-wow-duration="0.5s" data-wow-delay="0.2s">
+                                <div className="col-md-6 col-md-offset-6">
+                                    <div className="our-story-content wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s">
+                                        <div className="our-story-content-inner">
+                                            <h1>OUR STORY</h1>
+                                            <hr />
+                                            <p>
+                                                Great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally
+                                                        encounter consequences that are extremely painful. <br />
+                                                Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example.
+                                                    </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            
             </div>
         )
     }
--- src/Components/Pages/Portfolio.js
+++ src/Components/Pages/Portfolio.js
@@ -1,10 +1,102 @@
 import React, { Component } from 'react'
+import AppDispatcher from '../../Dispatcher/AppDispatcher'
+import AppStore from '../../Stores/AppStore'
+import ReactMarkdown from 'react-markdown'
 
 export default class Portfolio extends Component {
+    componentDidMount() {
+
+    }
+    componentWillMount() {
+        this.getPageData()
+    }
+    getPageData() {
+        AppDispatcher.dispatch({
+            action: 'get-page-data',
+            page_slug: 'Portfolio'
+        })
+    }
     render(){
+        const page = AppStore.data.page
+        console.log(page)
+        const title = page.fields.pageTitle
+        const subTitle = page.fields.subTitle
+        const photos = page.fields.photos
+        const image = page.fields.mainImage.fields.file.url
+        const header_style = {
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }
         return (
             <div>
-                Portfolio
+                
+                    <div className="container">
+                        <div className="header-page ef-parallax-bg" style={header_style}>
+                            <div className="col-md-6 col-md-offset-6">
+                                <div className="row">
+                                    <div className="inner-content">
+                                        <div className="header-content">
+                                            <h1>{title}</h1>
+                                            <hr/>
+                                            <p>{subTitle}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="container margin-top">
+                        <div className="portfolio-wrapper">
+                            <button className="nav">
+                                <span className="icon-container">
+                                <span className="line line01"></span>
+                                <span className="line line02"></span>
+                                <span className="line line03"></span>
+                                <span className="line line04"></span>
+                                </span>
+                            </button>
+                            <div className="works-filter">
+                                <a href="javascript:void(0)" className="filter active" data-filter="mix">All</a>
+                                <a href="javascript:void(0)" className="filter" data-filter="branding">Branding</a>
+                                <a href="javascript:void(0)" className="filter" data-filter="web">Web Design</a>
+                                <a href="javascript:void(0)" className="filter" data-filter="graphic">Graphic Design</a>
+                            </div>
+                            <div className="js-masonry">
+                                <div className="row" id="work-grid">
+                                    {photos.map(photo => {
+                                        return (
+                                            <div className="col-md-4 col-sm-4 col-xs-12 mix branding">
+                                                <div className="img home-portfolio-image">
+                                                    <img src={photo.fields.file.url} alt="Portfolio Item" />
+                                                    <div className="overlay-thumb">
+                                                        <a href="javascript:void(0)" className="like-product">
+                                                            <i className="ion-ios-heart-outline"></i>
+                                                            <span className="like-product">Liked</span>
+                                                            <span className="output">250</span>
+                                                        </a>
+                                                        <div className="details">
+                                                            <span className="title">STYLE FASHION</span>
+                                                            <span className="info">NEW BAG & STYLE FASHION</span>
+                                                        </div>
+                                                        <span className="btnBefore"></span>
+                                                        <span className="btnAfter"></span>
+                                                        <a className="main-portfolio-link" href="single-project.html"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                    
+                                </div>
+                            </div>
+                            <div className="load-more">
+                                <a href="javascript:void(0)" id="load-more"><i className="icon-refresh"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    
             </div>
         )
     }
